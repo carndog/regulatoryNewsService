a4fd1d4f8ba632e94fe20adbccc6589a6eceec24
Jason Carney <jason.carney@midlandhr.co.uk>
2019-06-28 15:33:45 +0100

    Configuration

--- RegulatoryNewsService/AppConfiguration.cs
+++ RegulatoryNewsService/AppConfiguration.cs
@@ -1,24 +1,43 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using Microsoft.Extensions.Configuration;
 
 namespace RegulatoryNewsService
 {
     public class AppConfiguration : IAppConfiguration
     {
-        private string[] _symbols;
-
         public AppConfiguration()
         {
-            ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
-            string path = Path.Combine(Directory.GetCurrentDirectory(), "appSettings.json");
-            configurationBuilder.AddJsonFile(path, false);
+            IConfigurationBuilder configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appSettings.json", false, true);
 
             IConfigurationRoot configurationRoot = configurationBuilder.Build();
-            IConfigurationSection configurationSection = configurationRoot.GetSection("Symbols").GetSection("Interest");
 
-            SymbolsConfiguration = new SymbolsConfiguration();
+            BuildSymbolsConfiguration(configurationRoot);
         }
 
-        public SymbolsConfiguration SymbolsConfiguration { get; }
+        private void BuildSymbolsConfiguration(IConfigurationRoot configurationRoot)
+        {
+            List<string> symbols = new List<string>(20);
+            string symbol = null;
+            int i = 0;
+            do
+            {
+                symbol = configurationRoot.GetSection("Symbols:Interest")[i++.ToString()];
+                if (symbol != null)
+                {
+                    symbols.Add(symbol);
+                }
+            }
+            while (symbol != null);
+
+            SymbolsConfiguration = new SymbolsConfiguration
+            {
+                Symbols = symbols.ToArray()
+            };
+        }
+
+        public SymbolsConfiguration SymbolsConfiguration { get; private set; }
     }
 }
